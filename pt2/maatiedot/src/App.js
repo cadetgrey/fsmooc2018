c1dfa3d84e888943cd5acbc0fa9cb8dc5d3d57d2
cadetgrey <maija.raudaskoski@gmail.com>
2018-12-15 23:31:27 +0200

    pt2ex12-13

--- pt2/maatiedot/src/App.js
+++ pt2/maatiedot/src/App.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
+class SearchEntry extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+  }
+
+  toggleExpansion = () => {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  render() {
+    const { name, capital, population, flag } = this.props.data
+    const showWhenExpanded = { display: this.state.expanded ? '' : 'none'}
+    return (
+      <div onClick={this.toggleExpansion}>
+        <h3>{name}</h3>
+        <div style={showWhenExpanded}>
+          <div>capital: {capital}</div>
+          <div>population: {population}</div>
+          <div><img src={flag} width='500'></img></div>
+        </div>
+      </div>
+    )
+  }
+}
+
 const SearchResultDisplay = ({ list, filterString }) => {
   const resultData =
     !filterString ?
       list :
-      list.filter(el => el.toLowerCase().includes(filterString.toLowerCase()))
+      list.filter(el => el.name.toLowerCase().includes(filterString.toLowerCase()))
 
   if (resultData.length > 10) {
     return <div>too many matches, specify another filter</div>
   }
 
+  if (resultData.length === 1) {
+    const country = resultData[0]
+    return (
+      <SearchEntry data={country}/>
+    )
+  }
+
   return (
     <div>
-      {resultData.map(el => <div>{el}</div>)}
+      {resultData.map(el => <SearchEntry data={el}/>)}
     </div>
   )
 }
@@ -31,7 +66,7 @@ class App extends Component {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        this.setState({ countries: response.data.map(el => el.name)});
+        this.setState({ countries: response.data.map(el => el) });
       })
   }
 
@@ -42,13 +77,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        find countries: 
+        find countries:
         <input
           value={this.state.searchTerm}
-          onChange={this.handleInputChange}/>
+          onChange={this.handleInputChange} />
         <SearchResultDisplay
           list={this.state.countries}
-          filterString={this.state.searchTerm}/>
+          filterString={this.state.searchTerm} />
       </div>
     );
   }
